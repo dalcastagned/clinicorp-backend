e27ba34ed03d9159d35900a7b2d5432ba0670563
Daniel Dalcastagne <dalcastagned@gmail.com>
2025-05-17 11:42:52 -0300

    refactor: padroniza mensagens de erro

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,12 +2,11 @@ const { StatusCodes } = require('http-status-codes')
 
 function errorHandler(err, _, res, __) {
   const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
-  const message = err.message || 'Ocorreu um erro interno no servidor.'
+  const details = err.message || 'Ocorreu um erro interno no servidor.'
 
   res.status(statusCode).json({
-    status: 'error',
-    statusCode,
-    message,
+    message: 'error',
+    details,
   })
 }
 
